b20efb204b880b770e0c95eaebde19178a4dfea1
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2023-12-25 17:05:13 +0400

    Update elseif.java

--- elseif.java
+++ elseif.java
@@ -1,24 +1,22 @@
 import java.util.Scanner;
- 
+
 public class elseif {
-   
-    public static void main(String[] args) {
-           
-        Scanner in = new Scanner(System.in); 
-        System.out.println("Vvedite pervoye chislo: ");
-        int a= in.nextInt();
-         
-        System.out.println("Vvedite vtoroye chislo: ");
-        int b= in.nextInt();
- 
-        if(a > b){
-            System.out.println("Pervoye chislo bolshe vtoroqo");
-        }
-        else if (a < b){
-            System.out.println("Pervoye chislo menshe vtoroqo");
-        }
-        else{
-            System.out.println("Oba chisla ravni");
-        }
+
+  public static void main(String[] args) {
+
+    Scanner in = new Scanner(System.in);
+    System.out.println("Vvedite pervoye chislo: ");
+    int a = in.nextInt();
+
+    System.out.println("Vvedite vtoroye chislo: ");
+    int b = in.nextInt();
+
+    if (a > b) {
+      System.out.println("Pervoye chislo bolshe vtoroqo");
+    } else if (a < b) {
+      System.out.println("Pervoye chislo menshe vtoroqo");
+    } else {
+      System.out.println("Oba chisla ravni");
     }
+  }
 }
