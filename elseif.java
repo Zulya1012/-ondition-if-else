d0ded0c44e54b9f4afc233014aae196bfc542f15
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2023-12-25 17:08:50 +0400

    Update elseif.java

--- elseif.java
+++ elseif.java
@@ -5,18 +5,18 @@ public class elseif {
   public static void main(String[] args) {
 
     Scanner in = new Scanner(System.in);
-    System.out.println("Vvedite pervoye chislo: ");
+    System.out.println("Enter first number: ");
     int a = in.nextInt();
 
-    System.out.println("Vvedite vtoroye chislo: ");
+    System.out.println("Enter second number: ");
     int b = in.nextInt();
 
     if (a > b) {
-      System.out.println("Pervoye chislo bolshe vtoroqo");
+      System.out.println("First number is greater than the second");
     } else if (a < b) {
-      System.out.println("Pervoye chislo menshe vtoroqo");
+      System.out.println("First number is less than the second");
     } else {
-      System.out.println("Oba chisla ravni");
+      System.out.println("Both numbers are equal");
     }
   }
 }
